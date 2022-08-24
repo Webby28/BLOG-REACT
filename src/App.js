5adcde9fbb1d5608f7e07efdda122d5ae8905c3c
Williams Baez <wbaez10@gmail.com>
2022-08-24 10:28:23 -0400

    Configurando settings de usuario

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import TopBar from "./components/topbar/TopBar";
 import Write from "./components/write/Write";
 import Home from "./pages/home/Home";  
+import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
 
 function App() {
   return (
     <>
     <TopBar />
-    <Write/>
+    <Settings/>
     </>
   );
 }
--- src/components/write/Write.jsx
+++ src/components/write/Write.jsx
@@ -3,10 +3,22 @@ import "./write.css"
 export default function Write() {
   return (
     <div className="write">
-      <form action="" className="writeFrom">
-        <div className="writeFromGroup">
-
+      <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Fondo_Dem%C3%B3crito_y_Her%C3%A1clito.jpg/800px-Fondo_Dem%C3%B3crito_y_Her%C3%A1clito.jpg?20201117112700" alt="" className="writeImg" />
+      <form action="" className="writeForm">
+        <div className="writeFormGroup">
+          <label htmlFor="fileInput">
+          <i className="writeIcon fa-solid fa-plus"></i>
+          </label>
+          <input type="file" id="fileInput" style={{display:"none"}}/>
+          <input type="text" placeholder="Title" className="writeInput" autoFocus={true} />
         </div>
+        <div className="writeFormGroup">
+          <textarea placeholder="Cuenta tu historia.." 
+          type="text" 
+          className="writeInput writeText">            
+          </textarea>
+        </div>
+        <button className="writeSubmit">Publicar</button>
       </form>
     </div>
   )
