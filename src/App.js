96a0a78c5e22fb7cc0e98305b865281c080a5e7e
Williams Baez <wbaez10@gmail.com>
2022-08-26 10:34:25 -0400

    agregando paths al sitio en App.js

--- src/App.js
+++ src/App.js
@@ -5,13 +5,27 @@ import Login from "./pages/login/Login";
 import Register from "./pages/register/Register";
 import Settings from "./pages/settings/Settings";
 import Single from "./pages/single/Single";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <>
+    
+    <Router> 
     <TopBar />
-    <Register/>
-    </>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          <Route path="/register" element={<Register/>} />
+          <Route path="/login" element={<Login/>} />
+          <Route path="/write" element={<Write/>} />
+          <Route path="/settings" element={<Settings/>} />
+          <Route path="/post/:postId" element={<Single/>} />
+        </Routes>
+    </Router>
   );
 }
 
