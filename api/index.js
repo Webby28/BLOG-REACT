254bd8a9af75d5409d7d03d1b243e76e31b94523
Williams Baez <wbaez10@gmail.com>
2022-08-28 19:47:13 -0400

    agregando conexion a mongoDB

--- api/index.js
+++ api/index.js
@@ -1,5 +1,16 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+   
+
+}). then(console.log("Conectado a MongoDB")).catch(err=>console.log(err));
 
 app.listen("5000", ()=> {
     console.log("Backend está ejecutandose.")
